b47fc86c6e6e75e9190738007457f7a90e7b54ad
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-04-27 12:27:57 -0400

    Se edito la entidad usuario correctamente, tambien aplicar a reportes

--- usuario-service/src/main/java/com/fullstack3/usuario_service/model/Usuario.java
+++ usuario-service/src/main/java/com/fullstack3/usuario_service/model/Usuario.java
@@ -1,4 +1,23 @@
 package com.fullstack3.usuario_service.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String run;
+
+    private String pasaporte;
+
 }
